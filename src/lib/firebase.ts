c0bffedb583472b42f8848b0fb3b13cc5bb69ac5
Mugdha Arekar <129976800+mugdhaarekar@users.noreply.github.com>
2026-03-26 17:39:44 +0530

    ADD

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,13 +11,12 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  clientSecret: import.meta.env.VITE_FIREBASE_CLIENT_SECRET,
+  apiKey: "AIzaSyBWaVUux5scga6t6AvM4ffpYmVM1VlWo_I",
+  authDomain:"healthcare-management-sy-b1c9b.firebaseapp.com",
+  projectId: "healthcare-management-sy-b1c9b",
+  storageBucket: "healthcare-management-sy-b1c9b.firebasestorage.app",
+  messagingSenderId: "363391179208",
+  appId: "1:363391179208:web:49c2a5196ab3f6cc3d48fd",
 };
 
 const app = initializeApp(firebaseConfig);
